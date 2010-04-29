2b5e747433fc04cec1b5f898af8e7521915a3e10
tdaniels <>
2010-04-29 00:33:53 +0000

    Remove PAT dependency from a Buildfile, slight modification to skim template file

--- Workspace/DataSkim/test/DataSkims_EarlyRunning_cfg.py
+++ Workspace/DataSkim/test/DataSkims_EarlyRunning_cfg.py
@@ -1,17 +1,26 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("TriggerSampleSkim")
+process = cms.Process("DILDO")
+process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
+process.load("Configuration.StandardSequences.GeometryExtended_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load('Configuration.EventContent.EventContent_cff')
+process.GlobalTag.globaltag = "GR10_E_V5::All"
+
+process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True)
-)
+    )
 #number of Events to be skimmed.
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
-)
+    )
 
 #replace fileNames  with the file you want to skim
 
-process.source = cms.Source("PoolSource")
+process.source = cms.Source("PoolSource",
+                            fileNames = cms.untracked.vstring()
+    )
 
 #load the EventContent and Skim cff/i files for EXOMu sub-skim.
 process.load('Workspace.data_skim.singleMuonSkim_EventContent_cfi')
@@ -24,6 +33,8 @@ process.load('Workspace.data_skim.electronTagProbe_EventContent_cfi')
 process.load('Workspace.data_skim.electronTagProbeFilters_cff')
 process.load('Workspace.data_skim.singlePhotonSkim_EventContent_cfi')
 process.load('Workspace.data_skim.singlePhotonSkim_cff')
+process.load('Workspace.data_skim.singlePfTauSkim_EventContent_cfi')
+process.load('Workspace.data_skim.singlePfTauSkim_cff')
 process.load('Workspace.data_skim.jetSkim_EventContent_cfi')
 process.load('Workspace.data_skim.jetSkim_cff')
 process.load('Workspace.data_skim.METSkim_EventContent_cfi')
@@ -60,6 +71,7 @@ process.load('Workspace.data_skim.commonCuts_cff')
 process.singleMuPt5SkimPath=cms.Path(process.singleMuPt5RecoQualitySeq)
 process.singleElectronPt5SkimPath=cms.Path(process.singleElectronPt5RecoQualitySeq)
 #process.superClusterPt5SkimPath=cms.Path(process.singleElectronSCRecoQualitySeq)
+process.singlePfTauPt15SkimPath=cms.Path(process.singlePfTauPt15QualitySeq)
 process.singlePhotonPt5SkimPath=cms.Path(process.singlePhotonPt5QualitySeq)
 process.muonJPsiMMSkimPath=cms.Path(process.muonJPsiMMRecoQualitySeq)
 process.jetSkimPath=cms.Path(process.jetRecoQualitySeq)
@@ -70,11 +82,18 @@ process.load('HLTrigger.special.HLTTriggerTypeFilter_cfi')
 #hltTriggerTypeFilter.SelectedTriggerType = 1
 process.hltTriggerTypeFilter.SelectedTriggerType = 1
 
+process.singleMuPt5OutputModule.fileName = cms.untracked.string('rfio:/castor/cern.ch/user/t/tdaniels/2010_TPG_SKIMS/ExpressPhysics/run133511/muonPt05Skim_1.root')
+process.singleElectronPt5OutputModule.fileName = cms.untracked.string('rfio:/castor/cern.ch/user/t/tdaniels/2010_TPG_SKIMS/ExpressPhysics/run133511/ElectronPt5Skim_1.root')
+process.jetOutputModule.fileName = cms.untracked.string('rfio:/castor/cern.ch/user/t/tdaniels/2010_TPG_SKIMS/ExpressPhysics/run133511/jetSkim_1.root')
+process.singlePhotonPt5OutputModule.fileName = cms.untracked.string('rfio:/castor/cern.ch/user/t/tdaniels/2010_TPG_SKIMS/ExpressPhysics/run133511/photonPt5Skim_1.root')
+process.singlePfTauPt15OutputModule.fileName = cms.untracked.string('rfio:/castor/cern.ch/user/t/tdaniels/2010_TPG_SKIMS/ExpressPhysics/run133511/pfTauPt15Skim_1.root')
+process.muonJPsiMMOutputModule.fileName = cms.untracked.string('rfio:/castor/cern.ch/user/t/tdaniels/2010_TPG_SKIMS/ExpressPhysics/run133511/muonJPsiMMSkim_1.root')
+
 process.endPath = cms.EndPath(process.hltTriggerTypeFilter*
 #                              process.primaryVertexFilter*
                               process.singleMuPt5OutputModule+
                               process.singleElectronPt5OutputModule+
-#                              process.superClusterPt5OutputModule+
+                              process.singlePfTauPt15OutputModule+
                               process.singlePhotonPt5OutputModule+                              
                               process.muonJPsiMMOutputModule+
                               process.jetOutputModule)
